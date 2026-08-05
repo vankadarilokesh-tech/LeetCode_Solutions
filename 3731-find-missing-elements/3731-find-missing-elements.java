a5394587669f2d159c4aff9e26d98bc1ba46f86f
Vankadari Lokesh <vankadarilokesh@gmail.com>
2026-08-05 22:11:45 +0530

    Time: 2 ms (91.93%), Space: 46.5 MB (91.19%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,17 +1,28 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        Arrays.sort(nums);
-        List<Integer> n = new ArrayList<>();
-         for(int i = 0; i < nums.length-1 ; i++){
-            int current = nums[i];
-            int next = nums[i+1];
-            while(current + 1 < next){
-                n.add(current+1);
-                current++;
+       List<Integer> n= new ArrayList<>();
+    for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]>nums[j]){
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
             }
-         }
-         return n;
-
+        }
+    }
+    int i=nums[0];
+    int j=0;
+    while(i<=nums[nums.length-1]){
+        if(i!=nums[j]){
+            n.add(i);
+        }
+        else{
+            j++;
+        }
+        i++;
+    }
+    return n;
+    
     }
 
 }
